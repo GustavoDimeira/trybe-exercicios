58bdb529ba86efcec7efad4081135ae178a35319
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-09-23 19:41:05 -0400

    Adicionando titulos

---
+++ back-end/dia-2.4/Adicionando 4 filmes.sql
@@ -0,0 +1,9 @@
+SELECT * FROM Pixar.Movies;
+
+INSERT INTO Pixar.Movies (title, director, `year`, length_minutes)
+VALUES 
+ ('Monstros SA', 'Pete Docter', 2001, 92),
+ ('Procurando Nemo', 'John Lasseter', 2003, 107),
+ ('Os Incríveis', 'Brad Bird', 2004, 116),
+ ('WALL-E', 'Pete Docter', 2008, 104)
+;
