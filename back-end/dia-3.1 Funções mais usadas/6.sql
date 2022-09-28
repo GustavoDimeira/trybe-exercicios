3b4088d1618c00292b9eccbc030ac56446f8d794
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-09-28 15:24:21 -0400

    6, quantos programadores tem

---
+++ back-end/dia-3.1 Funções mais usadas/6.sql
@@ -0,0 +1,11 @@
+USE hr;
+SELECT COUNT(*) FROM employees
+WHERE job_id = 'IT_PROG'
+;
+
+# IT_PROG -> id do programador
+
+/*
+Escreva uma query que exiba a quantidade de
+pessoas que trabalham como pessoas programadoras (IT_PROG).
+*/
