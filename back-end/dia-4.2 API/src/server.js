a7292f17deb3d2e63263438b81926e4495f18f9b
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-10-07 17:09:25 -0400

    funcao que pega um valor do objeto baseado na URL

--- back-end/dia-4.2 API/src/server.js
+++ back-end/dia-4.2 API/src/server.js
@@ -1,3 +1,10 @@
 const app = require('./app');
 
+const activities = require('./activities');
+
+app.get('/myActivities/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json(activities[Number(id) - 1]);
+});
+
 app.listen(3001, () => console.log('server running on port 3001'));
