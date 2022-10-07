cc66c16905b1297616b5e663915fd26e0745b67b
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-10-07 17:15:15 -0400

    listar todos os valores do array

--- back-end/dia-4.2 API/src/server.js
+++ back-end/dia-4.2 API/src/server.js
@@ -7,4 +7,8 @@ app.get('/myActivities/:id', (req, res) => {
   res.status(200).json(activities[Number(id) - 1]);
 });
 
+app.get('/myActivities', (req, res) => {
+  res.status(200).json(activities);
+});
+
 app.listen(3001, () => console.log('server running on port 3001'));
