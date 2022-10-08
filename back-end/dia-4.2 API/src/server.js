f4840ab6a7e520f4544a635325cc4933da9e83f5
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-10-08 16:13:42 -0400

    dia 4.2

--- back-end/dia-4.2 API/src/server.js
+++ back-end/dia-4.2 API/src/server.js
@@ -7,8 +7,19 @@ app.get('/myActivities/:id', (req, res) => {
   res.status(200).json(activities[Number(id) - 1]);
 });
 
-app.get('/myActivities', (req, res) => {
+app.get('/myActivities', (_req, res) => {
   res.status(200).json(activities);
+});
+
+app.get('/filter/myActivities', (req, res) => {
+  const { status } = req.query;
+  let filteredActivities = activities;
+
+  if (status) {
+    filteredActivities = activities.filter((activity) => activity.status === status);
+  }
+
+  res.status(200).json({ activities: filteredActivities });
 });
 
 app.listen(3001, () => console.log('server running on port 3001'));
