a67adb3be04a46ec3fbba0f96cd98f07ff69547f
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-15 14:20:40 -0400

    recursives functions

--- ciência da computação/modulo-4/2-dia/count_odd.py
+++ ciência da computação/modulo-4/2-dia/count_odd.py
@@ -3,6 +3,3 @@ Crie um algoritmo não recursivo para contar quantos números pares existem em u
 """
 
 def count_odd(n): return ValueError("N deve ser maior que 0") if (n < 0) else int(n/2)
-
-
-print(count_odd(-11))
