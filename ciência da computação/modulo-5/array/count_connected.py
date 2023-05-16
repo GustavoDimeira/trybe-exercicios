883d859790bc37a91bc0f6c9ef5c05c138c9100e
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-16 15:37:42 -0400

    fix

---
+++ ciência da computação/modulo-5/array/count_connected.py
@@ -0,0 +1,16 @@
+"""
+servidores =  [[1,0],[0,1]]
+resultado: 0
+Linhas e colunas são diferentes.
+
+servidores = [[1,0],[1,1]]
+resultado: 3
+Todos os servidores se comunicam com ao menos um outro servidor.
+
+servidores = [[1, 0, 0],
+              [1, 0, 0],
+              [0, 0, 1]]
+resultado: 2
+O servidor de índice (2, 2) não possui nenhum outro na mesma linha e coluna.
+"""
+
